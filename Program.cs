3d0ceb85a97b8e671fc34fc6bfb1a1dcb906cd83
hateteen <116436850+hateteen@users.noreply.github.com>
2022-11-24 09:52:50 +0300

    z27

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//void Zadacha25()
+﻿void Zadacha25()
 {
     Console.WriteLine("введите число A");
     int a = Convert.ToInt32(Console.ReadLine());
@@ -11,4 +11,24 @@
     step = step * a;
     }
     Console.WriteLine("A в степени B равно: " + step);
+}
+
+void Zadacha27()
+{
+    Console.WriteLine("введите число");
+    int i = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+
+    while (i > 0)
+    {
+    int num = i % 10;
+    i = i / 10;
+    sum = sum + num;
+    }
+    Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+}
+
+void Zadacha29()
+{
+
 }
