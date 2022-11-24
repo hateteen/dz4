65ffe61030af3c8b48dee35b29b17fe7b4161b01
hateteen <116436850+hateteen@users.noreply.github.com>
2022-11-24 09:50:50 +0300

    z25

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//void Zadacha25()
+{
+    Console.WriteLine("введите число A");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("введите число B");
+    int b = Convert.ToInt32(Console.ReadLine());
+    int step = a;
+
+    for (int i = 1; i < b; i++)
+    {
+    step = step * a;
+    }
+    Console.WriteLine("A в степени B равно: " + step);
+}
